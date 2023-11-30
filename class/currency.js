006c775e461a80b13fcf5399e7183a7b772d3caa
misheel <gumbee.1031@gmail.com>
2023-11-30 19:26:11 +0800

    11.30

---
+++ class/currency.js
@@ -0,0 +1,4 @@
+let balance = 0
+let currencyFrom = window.prompt("хөрвүүлэх мөнгөн тэмдэгт \n1. USD\n2. MNT\n3. CNY\n4. RUB");
+let value = window.prompt("end toon utga oruulna uu");
+let currencyTo = window.prompt("хөрвүүлэгдэх мөнгөн тэмдэгт \n1. USD\n2. MNT\n3. CNY\n4. RUB");
