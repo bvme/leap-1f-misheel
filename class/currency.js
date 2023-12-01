b50eb329722d57b002a10879126101da4607fdd0
misheel <gumbee.1031@gmail.com>
2023-12-01 19:17:42 +0800

    12.01

--- class/currency.js
+++ class/currency.js
@@ -1,4 +1,41 @@
-let balance = 0
-let currencyFrom = window.prompt("хөрвүүлэх мөнгөн тэмдэгт \n1. USD\n2. MNT\n3. CNY\n4. RUB");
-let value = window.prompt("end toon utga oruulna uu");
-let currencyTo = window.prompt("хөрвүүлэгдэх мөнгөн тэмдэгт \n1. USD\n2. MNT\n3. CNY\n4. RUB");
+let balance = 0;
+let currencyFrom = window.prompt(
+ "End temdegt oruulna uu ? \n1. USD\n2. MNT\n3. CNY\n4 .RUB"
+);
+switch (currencyFrom) {
+ case "1":
+ currencyFrom = 3450;
+ break;
+ case "2":
+ currencyFrom = 1;
+ break;
+ case "3":
+ currencyFrom = 480;
+ break;
+ case "4":
+ currencyFrom = 38;
+ break;
+ default:
+ window.alert ("buruu");
+}
+let value = window.prompt("End toon utga oruulna uu ?");
+let currencyTo = window.prompt(
+ "End temdegt oruulna uu ? \n1. USD\n2. MNT\n3. CNY\n4 .RUB"
+);
+switch (currencyTo) {
+ case "1":
+ currencyTo = 3450;
+ break;
+ case "2":
+ currencyTo = 1;
+ break;
+ case "3":
+ currencyTo = 480;
+ break;
+ case "4":
+ currencyTo = 38;
+ break;
+ default:
+ console.log("Buruu orolt");
+}
+window.alert((currencyFrom * value) / currencyTo);
---
+++ class/wending-machine.js
@@ -0,0 +1,63 @@
+// Hariult uguh devsgert 
+// 1. 50
+// 2. 100
+// 3. 500
+// 4. 1000
+// 5. 5000
+// 6. 10000
+
+let select_product = window.prompt(
+    "Buteegdehuunee songono uu ? \n1. cola = 1200₮\n2. sprite = 1500₮\n3. alpen gold = 2000₮\n4. suu = 2200₮\n5. talh = 1000₮\n6. zairmag = 500₮\n7. bohi = 150₮\n8. arhi = 15000₮"
+);
+switch (select_product) {
+    case "1" :
+        select_product = 1200;
+    break;
+    case "2" :
+        select_product = 1500;
+    break;
+    case "3" :
+        select_product = 2000;
+    break;
+    case "4" :
+        select_product = 2200;
+    break;
+    case "5" :
+        select_product = 1000;
+    break;
+    case "6" :
+        select_product = 500;
+    break;
+    case "7" :
+        select_product = 150;
+    break;
+    case "8" :
+        select_product = 15000;
+    break;
+    default:
+    window.alert("Сонгосон бараа байхгүй байна");
+}
+let cash = window.prompt("Mungun devsgert oruulna uu ? \n1. 5000\n2. 10000\n3. 20000");
+switch (cash) {
+    case "1" :
+        cash = 5000;
+    break;
+    case "2" :
+        cash = 10000;
+    break;
+    case "3" :
+        cash = 20000;
+    break;
+    default:
+    window.alert("хүлээн авах боломжгүй дэвсгэрт байна")
+}
+let change = cash - select_product
+do {
+    devsgert = change % 10000
+} while ( change - 10000)
+
+// if (cash > select_product){
+//     window.alert (`Таны бүтээгдэхүүн амжилттай захиалагдлаа. Таны хариулт ₮${cash - select_product}. Баярлалаа.`)
+// } else if (cash < select_product){
+//     window.alert ("Мөнгөн дэвсгэрт хүрэлцэхгүй байна")
+// }
